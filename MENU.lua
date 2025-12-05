89468857e61580ad24ab293ff49a92fc6fdce93a
MiguelCriadorDeScript <miguelamaral2292@gmail.com>
2025-12-05 12:39:50 -0300

    Remove HTML boilerplate from MENU.lua

--- MENU.lua
+++ MENU.lua
@@ -1,5 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -452,4 +450,3 @@ end</div>
         </div>
     </div>
 </body>
-</html>
